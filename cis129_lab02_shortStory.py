932985d64b50903fca82042d07f6e66cc3132ac9
Joshua <jfrandsen@mail.pima.edu>
2024-09-16 14:59:52 -0700

    Update cis129_lab02_shortStory.py with 5 print statement

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Joshua Frandsen
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+
+print(Once upon a time peanutbutter was very lonely.)
+print(So peanutbutter decided to go on an adventure.)
+print(Past the peanut fields he found the land of Jam)
+print(There he met Jelly and they became instant friends)
+print(Now peanutbutter and jelly are inseperable and live happliy ever after in my stomach)
