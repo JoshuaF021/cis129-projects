293e7d8e9b2b342ffe0dceeda48f80c456375c9c
Joshua <jfrandsen@mail.pima.edu>
2024-10-07 14:02:27 -0700

    Update CIS129_JoshuaFrandsen_Lab5.py

--- CIS129_JoshuaFrandsen_Lab5.py
+++ CIS129_JoshuaFrandsen_Lab5.py
@@ -15,4 +15,4 @@ todayBottles = 0  #This variable will store the number of bottles returned on a 
 totalPayout = 0   #This variable will store the calculated value of totalBottles times .10
 keepGoing = "y"   #This variable will be used to run the program again
 
-input(
+todayBottles = int(input(f"Enter number of bottles for day #{counter}:\n"))
