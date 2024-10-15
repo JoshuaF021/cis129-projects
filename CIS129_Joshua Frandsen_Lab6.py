7756a306772f804acb890a94d5d8eb652d69268f
Joshua <jfrandsen@mail.pima.edu>
2024-10-15 12:29:31 -0700

    Update CIS129_Joshua Frandsen_Lab6.py

--- CIS129_Joshua Frandsen_Lab6.py
+++ CIS129_Joshua Frandsen_Lab6.py
@@ -1,4 +1,10 @@
-"""Assume that hot dogs come in packages of 10, and hot dog buns come in packages of 8. Design a modular program that calculates the number of packages of hot dogs and the number of packages of hot dog buns needed for a cookout, with the minimum amount of leftovers. The program should ask the user for the number of people attending the cookout, and the number of hot dogs each person will be given. The program should display the following:
+"""Joshua Frandsen
+    CIS129 LAB 6
+    10/15/2024"""
+
+"""Assume that hot dogs come in packages of 10, and hot dog buns come in packages of 8. 
+Design a modular program that calculates the number of packages of hot dogs and the number of packages of hot dog buns needed for a cookout, with the minimum amount of leftovers. 
+The program should ask the user for the number of people attending the cookout, and the number of hot dogs each person will be given. The program should display the following:
 
     The minimum number of packages of hot dogs required.
     The minimum number of packages of buns required
@@ -7,32 +13,42 @@
 
 
 #Program Start
-Initialize totalHotDogs variable
-Call getTotalHotDogs function and assign it to totalHotDogs
-Call showResults, passing totalHotDogs as an argument
+#Set variables
+totalHotDogs = 0
 
-Function getTotalHotDogs:
-  Initialize attendees
-  Initialize hotDogs
-  Ask user for maximum attendees and assign to attendees
-  Ask user for designated hot dogs per attendee and assign to hotDogs
-  Total = attendees * hotDogs
-  Return total
+totalHotDogs = getTotalHotDogs() 
+showResults(totalHotDogs)
 
-Function showResults:
-  Initialize constant DOGS as 10
-  Initialize constant BUNS as 8
-  Initialize dogsLeft 
-  Initialize bunsLeft
-  Initialize minDogs 
-  Initialize minBuns 
-  Set dogsLeft as result of equation using DOGS, total and dogsLeft
-  Set minDogs as result of equation using total, DOGS and dogsLeft
-  Set bunsLeft as result of equation using BUNS, total and bunsLeft
-  Set minBuns as result of equation using total, BUNS and bunsLeft
-  Display "Minimum packages of hot dogs needed" along with the value of minDogs
-  Display "Minimum packages of hot dog buns needed" along with the value of minBuns
-  Display "Hot dogs remaining" along with the value of dogsLeft
-  Display "Hot dog buns remaining" along with the value of bunsLeft
+def getTotalHotDogs(totalHotDogs):
+    attendees = 0
+    hotDogs = 0
+    attendees = int(input("maximum attendees?"))
+    hotDogs = int(input("designated hot dogs per attendee?")) 
+    total = attendees * hotDogs
+    return total
 
-End of Program
+def showResults(totalHotDogs):
+    DOGS = 10
+    BUNS = 8
+    dogsLeft = 0 
+    bunsLeft = 0
+    minDogs = 0
+    minBuns = 0 
+    #Set dogsLeft as result of equation using DOGS, total and dogsLeft
+    dogsLeft = (DOGS - total % DOGS) % DOGS
+    #Set minDogs as result of equation using total, DOGS and dogsLeft
+    minDogs = (total / DOGS) + (0 ** (0 ** dogsLeft))
+    #Set bunsLeft as result of equation using BUNS, total and bunsLeft
+    bunsLeft = (BUNS - total % BUNS) % BUNS
+    #Set minBuns as result of equation using total, BUNS and bunsLeft
+    minBuns = (total / BUNS) + (0 ** (0 ** bunsLeft))
+
+    print("\nMinimum packages of hot dogs needed", minDogs)
+    print("Minimum packages of hot dog buns needed", minBuns)
+    print("Hot dogs remaining", int(dogsLeft))
+    print("Hot dog buns remaining", int(bunsLeft))
+
+
+
+
+#End of Program
