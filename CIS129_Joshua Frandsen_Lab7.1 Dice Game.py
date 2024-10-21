39f059bf0a7019f5d9a2bfedf0e0e198cf1673d6
Joshua <jfrandsen@mail.pima.edu>
2024-10-21 13:39:35 -0700

    Update CIS129_Joshua Frandsen_Lab7.1 Dice Game.py

--- CIS129_Joshua Frandsen_Lab7.1 Dice Game.py
+++ CIS129_Joshua Frandsen_Lab7.1 Dice Game.py
@@ -1,1 +1,56 @@
+""" Joshua Frandsen
+CIS129 Dice Game Lab 7
+10/21/2024"""
 
+# add libraries needed
+import random
+
+# the main function
+def main():
+print()
+
+# initialize variables
+endProgram == no
+playerOne == NO NAME
+playerTwo == NO NAME
+
+# call to inputNames
+playerOne, playerTwo = inputNames(playerOne, playerTwo)
+
+# while loop to run program again
+while endProgram == 'no':
+      # populate variables
+      winnersName == NO NAME
+      p1number = 0
+      p2number = 0
+  
+# call to rollDice
+winnerName = rollDice(p1number, p2number, playerOne, playerTwo, winnerName)
+
+# call to displayInfo
+displayInfo(winnerName)
+endProgram = input('Do you want to end program? (yes/no): ')
+
+#this function gets the players names
+inputNames(playerOne, playerTwo)
+          playerOne = input("Enter First Player Name:")
+          playerTwo = input("Enter Second Player Name:")
+          return playerOne, playerTwo
+
+#this function will get the random values
+rollDice(p1number, p2number, playerOne, playerTwo, winnerName)
+        p1number = random.randint(1, 6)
+        p2number = random.randint(1, 6)
+
+        if p1number = p2number
+           winnerName == TIE
+        elif p1number > p2number
+             winnerName == playerOne
+        else winnerName == playerTwo
+        return winnerName
+
+#this function displays the winner
+displayInfo(winnerName)
+            print(winnerName)
+# calls main
+main()
