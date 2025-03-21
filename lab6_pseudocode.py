cbb2924cf24795ac1211bc60de22aa27acf5e663
Joshua <jfrandsen@mail.pima.edu>
2025-03-21 12:10:07 -0700

    Update lab6_pseudocode.py

--- lab6_pseudocode.py
+++ lab6_pseudocode.py
@@ -16,90 +16,102 @@ number of hot dogs each person will be given. The program should display the fol
 4. The number of buns that will be left over
 
 Programming Exercise (Hotdog Cookout Calculator)
-"""
+
 
 // The following code is Pseudocode
 // Please convert the following code to Python Code and save as Lab6.py
 
-// main module                                 
-Module main()
-   // Local variable for the total number of hot dogs needed.
-   Declare Integer total
+// main module """                                
+def main():
+   #// Local variable for the total number of hot dogs needed.
+   #Declare Integer total
+   total = 0
 
-   // Input --------------------------------------
-   // Get the total number of hot dogs needed.
+   #input --------------------------------------
+   #// Get the total number of hot dogs needed.
    total = getTotalHotDogs()
 
-   // Processing ----------------------------------
-   // Named constants for the package sizes
-   Constant Integer DOGS = 10   // Hot dogs in a package
-   Constant Integer BUNS = 8    // Hot dog buns in a package
+   #// Processing ----------------------------------
+   #// Named constants for the package sizes
+   #Constant Integer 
+   DOGS = 10   #// Hot dogs in a package
+   #Constant Integer 
+   BUNS = 8    #// Hot dog buns in a package
 
-   // Local variables
-   Declare Integer dogsLeft  // Left over hot dogs
-   Declare Integer bunsLeft  // Left over hot dog buns
-   Declare Integer minDogs   // Minimum packages of hot dogs
-   Declare Integer minBuns   // Minimum packages of hot dog buns
+   #// Local variables
+   #Declare Integer 
+   bunsLeft = 0  #// Left over hot dog buns
+   #Declare Integer 
+   minDogs = 0  #// Minimum packages of hot dogs
+   #Declare Integer 
+   minBuns = 0  #// Minimum packages of hot dog buns
 
-   // Calculate the number of left over hot dogs.
-   Set dogsLeft = (DOGS - total MOD DOGS) MOD DOGS
+   #// Calculate the number of left over hot dogs.
+   #Set 
+   dogsLeft = (DOGS - total MOD DOGS) MOD DOGS
 
-   // Calculate the minimum number of packages of hot dogs.
-   Set minDogs = Math.ceil(total / DOGS) 
+   #// Calculate the minimum number of packages of hot dogs.
+   #Set 
+   minDogs = Math.ceil(total / DOGS) 
 
-   // Calculate the number of left over hot dog buns.
-   Set bunsLeft = (BUNS - total MOD BUNS) MOD BUNS
+   #// Calculate the number of left over hot dog buns.
+   #Set 
+   bunsLeft = (BUNS - total MOD BUNS) MOD BUNS
 
-   // Calculate the minimum number of packages of hot dogs buns.
-   Set minBuns = Math.ceil(total / BUNS) 
+   #// Calculate the minimum number of packages of hot dogs buns.
+   #Set 
+   minBuns = Math.ceil(total / BUNS) 
 
-   // Output ----------------------------------------
-   // Display the results.
-   Call showResults(dogsLeft, minDogs, bunsLeft, minBuns)
-End Module
+   #// Output ----------------------------------------
+   #// Display the results.
+   #Call 
+   showResults(dogsLeft, minDogs, bunsLeft, minBuns)
+#End Module
 
-// The getTotalHotDogs module gets the number of people
-// attending the cookout and the number of hot dogs each
-// person will be given, and stores the product in the
-// totalHotDogs reference variable.
+#// The getTotalHotDogs module gets the number of people
+#// attending the cookout and the number of hot dogs each
+#// person will be given, and stores the product in the
+#// totalHotDogs reference variable.
 
-Function Integer getTotalHotDogs()
+#Function Integer 
+def getTotalHotDogs():
 
-   // Local variables
-   Declare Integer people   // Number of people attending
-   Declare Integer hotDogs  // Hot dogs per person
+   #// Local variables
+   #Declare Integer 
+   people = 0   #// Number of people attending
+   #Declare Integer 
+   hotDogs = 0 #// Hot dogs per person
 
-   // Get the number of people attending the cookout.
-   Display "Enter the number of people attending the cookout: "
-   Input people
+   #// Get the number of people attending the cookout.
+   people = input("Enter the number of people attending the cookout: ")
+   #Input people
 
-   // Get the number of hot dogs each person will be given.
-   Display " Enter the number of hot dogs for each person: "
-   Input hotDogs
+   #// Get the number of hot dogs each person will be given.
+   hotDogs = input(" Enter the number of hot dogs for each person: ")
+   #Input hotDogs
 
-   // Calculate the total number of hot dogs needed.
+   #// Calculate the total number of hot dogs needed.
    total = people * hotDogs
    return total
-End Function
+#End Function
 
-// The showResults module accepts the total number of hot dogs
-// as an argument and calculates the minimum packages of hot
-// dogs and hot dog buns, the left over hot dogs and hot dog
-// buns, and displays the results.
+#// as an argument and calculates the minimum packages of hot
+#// dogs and hot dog buns, the left over hot dogs and hot dog
+#// buns, and displays the results.
 
-Module ShowResults(Integer dogsLeft, Integer minDogs, 
-                   Integer bunsLeft, Integer minBuns)
+#Module 
+def ShowResults(dogsLeft, minDogs, bunsLeft, minBuns):
 
-   // Display the minimum packages of hot dogs needed.
-   Display "Minimum packages of hot dogs needed: ", minDogs
+   #// Display the minimum packages of hot dogs needed.
+   print("Minimum packages of hot dogs needed: ", minDogs)
 
-   // Display the minimum packages of buns needed.
-   Display "Minimum packages of hot dog buns needed: ", minBuns
+   #// Display the minimum packages of buns needed.
+   print("Minimum packages of hot dog buns needed: ", minBuns)
 
-   // Display the number of hot dogs left over.
-   Display "Hot dogs left over: ", dogsLeft
+   #// Display the number of hot dogs left over.
+   print("Hot dogs left over: ", dogsLeft)
 
-   // Display the number of hot dog buns left over.
-   Display "Hot dog buns left over: ", bunsLeft
-End Module
+   #// Display the number of hot dog buns left over.
+   print("Hot dog buns left over: ", bunsLeft)
+#End Module
 
